15d60b4367a93f05f19f7cefc84ac33d47b42609
zd07 <zahidibnrizwan@gmail.com>
2024-10-27 15:02:00 +0530

    breadcums updated

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,36 +1,188 @@
-import { useState } from 'react'
-import './App.css'
-import QuizPage from './pages/QuizPage';
-import Sidebar from './components/Sidebar'
-import Header from './components/Header'
+// import { useState } from 'react'
+// import './App.css'
+// import QuizPage from './pages/QuizPage';
+// import Sidebar from './components/Sidebar'
+// import Header from './components/Header'
+// import Blackboard from './components/Blackboard';
+
+// function App() {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+//   // Toggle Sidebar function
+//   const toggleSidebar = () => {
+//     setIsSidebarOpen(!isSidebarOpen);
+//   };
+
+//   return (
+//       <>
+//     <div className="flex h-screen bg-white overflow-hidden">
+   
+//       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+
+//       <div className="flex-1 flex flex-col">
+//         <Header toggleSidebar={toggleSidebar} />
+//         <main className="flex-1 p-4 bg-gray-100">
+//           <h1 className="text-2xl font-bold">Student Dashboard</h1>
+    
+//         </main>
+//       </div>
+//     </div>
+  
+//     <QuizPage/>
+//     <Blackboard/>
+//     </>
+//   );
+// }
+
+// export default App
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router';
+
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import SignIn from './pages/Authentication/SingIn';
+import SignUp from './pages/Authentication/SignUp';
+import Calendar from './pages/Calendar';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
+import DefaultLayout from './layout/DefaultLayout';
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { pathname } = useLocation();
 
-  // Toggle Sidebar function
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return (
-      <>
-    <div className="flex h-screen bg-white overflow-hidden">
-      {/* Sidebar */}
-      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
 
-      {/* Main content */}
-      <div className="flex-1 flex flex-col">
-        <Header toggleSidebar={toggleSidebar} />
-        <main className="flex-1 p-4 bg-gray-100">
-          <h1 className="text-2xl font-bold">Student Dashboard</h1>
-          {/* Add your content here */}
-        </main>
-      </div>
-    </div>
-  
-    {/* <QuizPage/> */}
-    </>
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <ECommerce />
+            </>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Calendar />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Profile />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-elements"
+          element={
+            <>
+              <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <FormElements />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-layout"
+          element={
+            <>
+              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <FormLayout />
+            </>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Settings />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Chart />
+            </>
+          }
+        />
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Alerts />
+            </>
+          }
+        />
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Buttons />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignIn />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignUp />
+            </>
+          }
+        />
+      </Routes>
+    </DefaultLayout>
   );
 }
 
-export default App
+export default App;
